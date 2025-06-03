0fd8ba80e18eca2d81a602b1c8741bab25a5bb66
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-06-03 14:54:46 -0400

    Update role to install and enable systemd-timesyncd

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,7 +12,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+@pytest.mark.parametrize("pkg", ["systemd-timesyncd"])
+def test_packages(host, pkg):
+    """Verify that the expected packages are installed."""
+    assert host.package(pkg).is_installed, f"The package {pkg} is not installed."
+
+
+@pytest.mark.parametrize("svc", ["systemd-timesyncd.service"])
+def test_services(host, svc):
+    """Verify that the expected services are present."""
+    s = host.service(svc)
+    assert s.exists, f"{svc} service does not exist."
+    assert s.is_enabled, f"{svc} service is not enabled."
